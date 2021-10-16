61512274e44fd5086dc5c5a3f3a460213127df77
BooLooBooLoo <lethug451@gmail.com>
2021-10-16 08:40:57 +0200

    Le Menu

---
+++ Assets/Scripts/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadCreditScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadCharactersScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}

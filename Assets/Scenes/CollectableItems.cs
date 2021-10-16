68f0b18e78f07babc8f594bee8d51dd58548731a
Noé Landré <noe.landre@laposte.net>
2021-10-16 09:51:46 +0200

    Add CollectableItems script

---
+++ Assets/Scenes/CollectableItems.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CollectableItems : MonoBehaviour
+{
+    [SerializedField] public string ItemName;
+    [SerializedField] public string ItemBuffName;
+    [SerializedField] public string ItemBuffValue;
+}
--- Assets/Scenes/Movement.cs
+++ Assets/Scenes/Movement.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.Json;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,15 +9,21 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] public float speed = 20.0f;
     [SerializeField] public bool isPlayer1 = true;
+    [SerializeField] public int health = 200;
+    [SerializeField] public int maxHealth = 200;
 
     private Rigidbody2D rb;
     private GameInputs inputs;
     private InputAction movement;
+    private List itemList;
+    private dynamic itemDictionary;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         inputs = new GameInputs();
+        string json = System.IO.File.ReadAllText("CollectableItems.json");
+        itemDictionary = JsonConvert.DeserializeObject(json);
     }
 
     private void OnEnable()
@@ -41,5 +48,37 @@ public class Movement : MonoBehaviour
         float horizontalInput = axisInput.x;
         float verticalInput = axisInput.y;
         rb.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, rb.velocity.y);
+        if(horizontalInput > 0){
+            transform.rotate(new Vector3(0, 180, 0));
+        }else{
+            transform.rotate(new Vector3(0, 0, 0));
+        }
+        
+    }
+
+    // Collect item when walking on it 
+    public void CollectItem(string name)
+    {
+        itemList.Add(name);
+    }
+
+    // Add effect according to item
+    public void AddItemEffect()
+    {
+        foreach(string name in itemList){
+            foreach(object o in itemDictionary){
+                if(o.name == name){
+                    switch (o.buff["name"])
+                    {
+                        case "PV":
+                            health += o.buff["value"];
+                            maxHealth += o.buff["value"];
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
     }
 }

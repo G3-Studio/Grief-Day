bb9ed6d619ae9a1cc7725a4842f19b480d620dac
Vincent Gonnet <vincent.gonnet08@gmail.com>
2022-03-05 12:59:34 +0100

    created prefab and added functions

--- Assets/Scripts/ItemSpawn.cs
+++ Assets/Scripts/ItemSpawn.cs
@@ -5,13 +5,9 @@ using System;
 
 public class ItemSpawn : MonoBehaviour
 {
-    [SerializeField] GameObject prefab;
-    private int x = 0;
-    private float y;
-    private int j = 0;
-    private int[] antiStacker = new int[4];
-    private bool validatedX;
-    float[] variables = new float[4] { -1.2f, 4.6f, 10.2f, 15.8f};
+    [SerializeField] GameObject coinPrefab;
+    [SerializeField] GameObject buffPrefab;
+    float[] yOffsets = new float[4] { -1.2f, 4.6f, 10.2f, 15.8f};
 
     void Awake() {
         GameManager.OnGameStateChanged += GameManagerOnGameStateChanged;
@@ -21,7 +17,11 @@ public class ItemSpawn : MonoBehaviour
     }
 
     private void GameManagerOnGameStateChanged(GameState state) {
-        if(state == GameState.AngerCoins) {
+        if(state == GameState.Deny) {
+            InvokeRepeating("SpawnBuffs", 0, 20);
+        } else if (state == GameState.AngerCoins) {
+            CancelInvoke("SpawnBuffs");
+            ClearBuffs();
             InvokeRepeating("SpawnCoins", 0, 20);
         } else {
             CancelInvoke("SpawnCoins");
@@ -31,6 +31,12 @@ public class ItemSpawn : MonoBehaviour
     }
     void SpawnCoins()
     {
+        int x = 0;
+        float y;
+        int j = 0;
+        int[] antiStacker = new int[4];
+        bool validatedX;
+
         for (int i =0; i<4; i++)
         {
             
@@ -48,8 +54,39 @@ public class ItemSpawn : MonoBehaviour
                 }
                 antiStacker[j] = x;
 
-                y = variables[i];
-                GameObject item = (GameObject)Instantiate(prefab, new Vector3(x, y, 0), transform.rotation, gameObject.transform);
+                y = yOffsets[i];
+                GameObject item = (GameObject)Instantiate(coinPrefab, new Vector3(x, y, 0), transform.rotation, gameObject.transform);
+                j++;
+            }
+        }
+            
+    }
+    void SpawnBuffs()
+    {
+        int x = 0;
+        float y;
+        int j = 0;
+        int[] antiStacker = new int[2];
+        bool validatedX;
+
+        for (int i =0; i<4; i++)
+        {
+            j = 0;
+            Array.Clear(antiStacker, 0, antiStacker.Length);   
+            while (j != 2)
+            {
+                validatedX = false;
+                while(!validatedX) {
+                    x = UnityEngine.Random.Range(-31, 9);
+                    foreach(int prevPos in antiStacker) {
+                        validatedX = (Math.Abs(prevPos - x) > 10);
+                        if (!validatedX) break;
+                    } // if validateX is false for one value, stays false, else become true
+                }
+                antiStacker[j] = x;
+
+                y = yOffsets[i];
+                GameObject item = (GameObject)Instantiate(buffPrefab, new Vector3(x, y, 0), transform.rotation, gameObject.transform);
                 j++;
             }
         }
@@ -60,6 +97,12 @@ public class ItemSpawn : MonoBehaviour
         GameObject[] coins = GameObject.FindGameObjectsWithTag("Coins");
         foreach(GameObject coin in coins) {
             GameObject.Destroy(coin);
+        }
+    }
+    void ClearBuffs() {
+        GameObject[] buffs = GameObject.FindGameObjectsWithTag("Buffs");
+        foreach(GameObject buff in buffs) {
+            GameObject.Destroy(buff);
         }
     }
 }

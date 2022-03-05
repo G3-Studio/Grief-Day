ff95905a18299f36147fda74bb78f6c91b84c06f
Noé Landré <68849024+DevNono@users.noreply.github.com>
2022-03-05 12:58:52 +0100

    Fix/change camera offset (#20)
    * fix: camera y axis
    
    * fix: items list
    
    * fix: type

--- Assets/Scripts/Utils/JsonTypes.cs
+++ Assets/Scripts/Utils/JsonTypes.cs
@@ -5,29 +5,34 @@ namespace Utils
 
     class JsonUtils
     {
-        public static T[] LoadJsonArray<T>(string json) {
-            return UnityEngine.JsonUtility.FromJson<WrapperJson<T>>("{\"items\":" + json + "}").items;
-        }
-
-        [Serializable]
-        public class WrapperJson<T>
-        {
-            public T[] items;
+        public static CollectableItemJson[] LoadJson<CollectableItemJson> (string json) {
+            return UnityEngine.JsonUtility.FromJson<CollectableItemJson[]>(json);
         }
 
         [Serializable]
         public class CollectableItemJson
-        {
-            public string name;
-            public BuffJson buff;
-            
+        {   
+            public Buff buff;
+
             [Serializable]
-            public class BuffJson
-            {
+            public class Buff {
                 public string name;
-                public int value;
+                public BuffJson buff;
+                
+                [Serializable]
+                public class BuffJson
+                {
+                    public string name;
+                    public int value;
+                }
             }
             
+            public Skills skill;
+
+            [Serializable]
+            public class Skills{
+                public string name;
+            }
         }
 
         

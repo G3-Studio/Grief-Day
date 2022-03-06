8d84a968e4b5693b735d7732bf37b1dd1cb397a1
Teddy Roncin <teddy.roncin35@gmail.com>
2022-03-06 12:36:47 +0100

    Removed Debug.Log()

--- Assets/Scripts/Player/SelectSkill.cs
+++ Assets/Scripts/Player/SelectSkill.cs
@@ -12,21 +12,17 @@ public class SelectSkill : MonoBehaviour
             return;
         }
 
-        Debug.Log("Trying select skill");
-
         gameObject.GetComponent<Player>().inventory.AddSkill(selectedShrine.GetComponent<Shrine>().skill);
         Destroy(selectedShrine);
         GameObject.Find("ItemSpawner").GetComponent<ItemChoosing>().skillSelected();
     }
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Shrine") {
-            Debug.Log("Entering shrine");
             selectedShrine = other.gameObject;
         } 
     }
     private void OnTriggerExit2D(Collider2D other) {
         if (other.tag == "Shrine") {
-            Debug.Log("Exiting shrine");
             selectedShrine = null;
         }
     }

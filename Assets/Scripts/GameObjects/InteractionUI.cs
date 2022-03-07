c116d93a82ef089ad22f75860aa3059a8807884d
Vincent Gonnet <vincent.gonnet08@gmail.com>
2022-03-07 09:15:12 +0100

    adjusted box position, works on both players

--- Assets/Scripts/GameObjects/InteractionUI.cs
+++ Assets/Scripts/GameObjects/InteractionUI.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class InteractionUI : MonoBehaviour
 {
-    private bool hasCollided = false;
-    private string labelText = "";
+    private bool hasCollidedP1 = false;
+    private bool hasCollidedP2 = false;
+    private string labelText = "Interact";
+    private static float boxWidth = 70;
+    private Rect player1Rect = new Rect((Screen.width / 2) - (boxWidth + 20), Screen.height - 40, boxWidth, 20);
+    private Rect player2Rect = new Rect((Screen.width / 2) + 20, Screen.height - 40, boxWidth, 20);
  
     void OnGUI()
-        {
-            if (hasCollided ==true)
-        {    
-            GUI.Box(new Rect(140,Screen.height-50,Screen.width-300,120),(labelText));
-        }
+    {
+        if (hasCollidedP1 ==true) GUI.Box(player1Rect,(labelText));
+        if (hasCollidedP2 ==true) GUI.Box(player2Rect,(labelText));
     }
  
     void OnTriggerEnter2D(Collider2D c)
      {
-        if(c.gameObject.tag =="Player") {
-            hasCollided = true;
-            labelText = "Hit E to pick up the key!";
-        }
+        if(c.gameObject.name =="Player 1") hasCollidedP1 = true;
+        if(c.gameObject.name =="Player 2") hasCollidedP2 = true;
         
     }
     
     void OnTriggerExit2D( Collider2D c ) {
-      if(c.gameObject.tag =="Player") hasCollided = false;
+      if(c.gameObject.name =="Player 1") hasCollidedP1 = false;
+      if(c.gameObject.name =="Player 2") hasCollidedP2 = false;
     }
 }

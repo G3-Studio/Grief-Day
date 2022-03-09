e7396aec2381a91a2d3f17977583b92830712c7e
Noé Landré <noe.landre@laposte.net>
2022-03-09 19:39:05 +0100

    fix: update & remove packages

--- Assets/PlayerEffectUI.cs
+++ Assets/PlayerEffectUI.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using Utils;

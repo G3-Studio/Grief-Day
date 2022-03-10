719f7a4d85e39aaefb8c75f84959d263c24b8aec
Noé Landré <68849024+DevNono@users.noreply.github.com>
2022-03-10 13:19:37 +0100

    Revert "Version Beta 1 (#46)" (#47)
    This reverts commit 7dedbe400996f6ad3b903fb1fd143a0998b2d9d5.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] Transform target;  // The player position
+    [SerializeField] Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.position.x, target.position.y + 2.3f, target.position.z); 
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
     }
 }

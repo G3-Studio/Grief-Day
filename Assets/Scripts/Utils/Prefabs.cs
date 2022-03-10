07df83bd78df53c0a26c60efcad8a4253052d5ee
Teddy Roncin <teddy.roncin35@gmail.com>
2022-03-10 11:38:20 +0100

    started to implement demon

---
+++ Assets/Scripts/Utils/Prefabs.cs
@@ -0,0 +1,10 @@
+﻿using System.Resources;
+using UnityEngine;
+
+namespace Utils {
+    
+    public class Prefabs {
+        public static readonly GameObject DEMON = Resources.Load<GameObject>("Prefabs/demon");
+    }
+    
+}

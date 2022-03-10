8d20a91dd35099f51f46d43aa230f84c6a1707eb
Noé Landré <68849024+DevNono@users.noreply.github.com>
2022-03-10 13:19:22 +0100

    Revert "Version Beta 1 (#46)"
    This reverts commit 7dedbe400996f6ad3b903fb1fd143a0998b2d9d5.

---
+++ Assets/Scripts/CollectableItems.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CollectableItems : MonoBehaviour
+{
+    [SerializeField] public string ItemName;
+    [SerializeField] public string ItemBuffName;
+    [SerializeField] public string ItemBuffValue;
+}
--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-Grief's Day by G³ Studio
-
-[![Unity Deploy](https://github.com/G3-Studio/Grief-Day/actions/workflows/unity-builder.yml/badge.svg)](https://github.com/G3-Studio/Grief-Day/actions/workflows/unity-builder.yml)
-
-Our first game
+Grief Day

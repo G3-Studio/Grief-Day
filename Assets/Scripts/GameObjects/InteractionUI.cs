4e3338c86905769eb8cd0e45602f742ea2658168
AyoubAzzz <ayoublines@gmail.com>
2022-04-03 11:34:02 +0200

    Background complete
    Background complete +  stairs reorganization

--- Assets/Scripts/GameObjects/InteractionUI.cs
+++ Assets/Scripts/GameObjects/InteractionUI.cs
@@ -1,24 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractionUI : MonoBehaviour
 {
     private bool hasCollidedP1 = false;
     private bool hasCollidedP2 = false;
     private string labelText = "Interact";
+
+    
     private static float boxWidth = 70;
-    private Rect player1Rect = new Rect((Screen.width / 2) - (boxWidth + 20), Screen.height - 40, boxWidth, 20);
-    private Rect player2Rect = new Rect((Screen.width / 2) + 20, Screen.height - 40, boxWidth, 20);
- 
+    private Rect player1Rect = new Rect((Screen.width / 2) - (boxWidth + 300), Screen.height -100, boxWidth,100 );
+    private Rect player2Rect = new Rect((Screen.width / 2) + 90, Screen.height- 110 , boxWidth, 100);
+     [SerializeField] Font GUIfont;
+        
+    
     void OnGUI()
     {
-        if (hasCollidedP1 ==true) GUI.Box(player1Rect,(labelText));
-        if (hasCollidedP2 ==true) GUI.Box(player2Rect,(labelText));
+         GUIStyle myStyle = new GUIStyle(); 
+         myStyle.fontSize = 100;
+         myStyle.font= GUIfont;
+         myStyle.normal.textColor= new Color(255,255,255) ;
+        
+         
+        if (hasCollidedP1 ==true) {
+             
+                
+                GUI.Box(player1Rect,(labelText),myStyle);
+        } 
+        if (hasCollidedP2 ==true){
+               
+                GUI.Box(player2Rect,(labelText),myStyle);
+        } 
     }
  
     void OnTriggerEnter2D(Collider2D c)
      {
+         
         if(c.gameObject.name =="Player 1") hasCollidedP1 = true;
         if(c.gameObject.name =="Player 2") hasCollidedP2 = true;
         
@@ -28,4 +49,5 @@ public class InteractionUI : MonoBehaviour
       if(c.gameObject.name =="Player 1") hasCollidedP1 = false;
       if(c.gameObject.name =="Player 2") hasCollidedP2 = false;
     }
+   
 }

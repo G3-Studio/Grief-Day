a2a7bcd559df742ffc415705bab9a689783cf5f0
Teddy Roncin <teddy.roncin35@gmail.com>
2022-04-04 11:49:25 +0200

    When all skills have been used, the list of skills is refilled

--- Assets/Scripts/Skills.cs
+++ Assets/Scripts/Skills.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Skills
 {
+    private static List<JsonUtils.CollectableItemJson.Skill> allSkills;
     public static List<JsonUtils.CollectableItemJson.Skill> skills;
     public JsonUtils.CollectableItemJson.Skill skill;
 
     static Skills() {
         string json = Resources.Load<TextAsset>("JSON/CollectableItems").text;
-        skills = JsonUtils.LoadJson<JsonUtils.CollectableItemJson>(json).skill;
+
+        allSkills = JsonUtils.LoadJson<JsonUtils.CollectableItemJson>(json).skill;
+        skills = new List<JsonUtils.CollectableItemJson.Skill>(allSkills);
     }
     public static JsonUtils.CollectableItemJson.Skill getSkill() 
     {
         JsonUtils.CollectableItemJson.Skill selectedSkill = skills[Random.Range(0, skills.Count)];
         skills.Remove(selectedSkill);
 
+        // For the moment, we don't have enough skills, so to avoid crashes, we refill the list. That is NOT a long term solution
+        if (skills.Count == 0) {
+            skills = new List<JsonUtils.CollectableItemJson.Skill>(allSkills);
+        }
         return selectedSkill;
     }
 

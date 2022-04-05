553717ea672afb56d4b002d3512462320df2192f
Teddy Roncin <teddy.roncin35@gmail.com>
2022-04-06 01:53:34 +0200

    Revert "This should work, but FOR SOME REASON this=null. Excuse me ??"
    This reverts commit 51e06b97efaf02f73e965e8127ddf4a8e9bcd8b4.

--- Assets/CountText.cs
+++ Assets/CountText.cs
@@ -7,29 +7,9 @@ public class CountText : MonoBehaviour
     TextMeshProUGUI countDisplay;
     CollectableItems counterPlayer;
     private int countPlayer = 0;
-    private bool enabled = false;
-
-    public CountText()
-    {
-        Debug.Log("on initialise !");
-        GameManager.OnGameStateChanged += this.OnGameStateChanged;
-    }
-
-    private void OnGameStateChanged(GameState gameState) {
-        Debug.Log("ça change, on a : " + gameState);
-        Debug.Log(this);
-        if (gameState == GameState.AngerCoins) {
-            this.transform.GetChild(0).gameObject.SetActive(true);
-            this.transform.GetChild(1).gameObject.SetActive(true);
-        } else {
-            this.transform.GetChild(0).gameObject.SetActive(false);
-            this.transform.GetChild(1).gameObject.SetActive(false);
-        }
-    }
-    
     private void Awake() 
     {
-        countDisplay = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        countDisplay = gameObject.GetComponent<TextMeshProUGUI>();
     }
 
     void Update()
